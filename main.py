58c83828e5b6108114353aa10b83ba7da4a14575
mariiiiiinad <118826092+mariiiiiinad@users.noreply.github.com>
2023-04-10 15:40:37 +0300

    доработала, зациклила кнопки

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 from config import BOT_TOKEN
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import telebot
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -11,35 +11,50 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [['/hello', '/start'],
-                  ['/rating', '/bye']]
+reply_keyboard = [['/start']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
 async def hello(update, context):
+    global reply_keyboard, markup
+    reply_keyboard = [['/start_game'], ['/rating', '/bye']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text('приветик(приветик)', reply_markup=markup)
+    return 1
 
 
 async def start_game(update, context):
-    await update.message.reply_text("start", reply_markup=markup)
-
+    await update.message.reply_text("start_game", reply_markup=markup)
+    return 2
 
 async def rating(update, context):
     await update.message.reply_text("rating", reply_markup=markup)
-
+    return 3
 
 async def bye(update, context):
+    global reply_keyboard, markup
+    reply_keyboard = [['/start']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text("bye", reply_markup=markup)
+    return ConversationHandler.END
 
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', hello)],
+    states={
+        1: [MessageHandler(filters.Regex("start_game"), start_game)],
+        2: [MessageHandler(filters.Regex("rating"), rating)]
+    },
+    fallbacks=[CommandHandler('bye', bye)]
+)
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("hello", hello))
-    application.add_handler(CommandHandler("start", start_game))
+    application.add_handler(CommandHandler("start", hello))
+    application.add_handler(CommandHandler("start_game", start_game))
     application.add_handler(CommandHandler("rating", rating))
     application.add_handler(CommandHandler("bye", bye))
-    text_handler = MessageHandler(filters.TEXT, hello)
-    application.add_handler(text_handler)
+    application.add_handler(conv_handler)
     application.run_polling()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
